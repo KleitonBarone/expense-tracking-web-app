82d03d1f4fdd21f714fd6aa0c8720be548592efb
KleitonBarone <25616578+KleitonBarone@users.noreply.github.com>
2024-10-06 16:51:26 -0300

    feat: add serving static files from backend

--- backend/app.ts
+++ backend/app.ts
@@ -2,15 +2,23 @@ import { Hono } from "hono";
 import { logger } from "hono/logger";
 
 import { expensesRoutes } from "./routes/expenses.ts";
+import { serveStatic } from "hono/bun";
 
 const app = new Hono();
 
 app.use("*", logger());
 
-app.get("/", (context) => {
-  return context.text("Hello World!");
-});
-
 app.route("/api/v1/expenses", expensesRoutes);
+
+app.get(
+  "*",
+  serveStatic({
+    root: "../frontend/dist",
+  })
+);
+
+app.notFound((context) => {
+    return context.redirect("/");
+});
 
 export default app;
